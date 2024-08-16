75593bfed157fb78a5789d4e3e823e664febb490
Litika-Gopinathan <114471493+Litika-Gopinathan@users.noreply.github.com>
2024-08-16 16:10:46 +0530

    Add files via upload

---
+++ Week_3_Exercises/Ex-2 Employee Management System - Creating Entities/src/main/java/com/example/EmployeeManagementSystem/Employee.java
@@ -0,0 +1,28 @@
+package com.example.EmployeeManagementSystem;
+
+import lombok.Data;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.JoinColumn;
+
+
+@Data
+@Entity
+@Table(name = "employees")
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+
+    @ManyToOne
+    @JoinColumn(name = "department_id")
+    private Department department;
+}
---
+++ Week_3_Exercises/Ex-3 Employee Management System - Creating Repositories/src/main/java/com/example/EmployeeManagementSystem/EmployeeRepository.java
@@ -0,0 +1,8 @@
+package com.example.EmployeeManagementSystem;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+    Optional<Employee> findByEmail(String email);
+}
