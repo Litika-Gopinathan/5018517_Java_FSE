c4ad92b042210fba8025ae410ca713b471888a54
Litika-Gopinathan <114471493+Litika-Gopinathan@users.noreply.github.com>
2024-08-16 16:25:22 +0530

    Delete Week_2_Exercises directory

--- Week_2_Exercises/Spring Core Maven/Ex-1 Configuring a Basic Spring Application/src/main/java/com/library/service/BookService.java
+++
@@ -1,8 +0,0 @@
-package com.library.service;
-
-public class BookService {
-    public void addBook() {
-        System.out.println("Adding a book to the library.");
-    }
-}
-
--- Week_2_Exercises/Spring Core Maven/Ex-5 Configuring the Spring IoC Container/src/main/java/com/library/repository/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package com.library.repository;
-
-public class BookRepository {
-    public void save() {
-        System.out.println("Saving a book to the database.");
-    }
-}
