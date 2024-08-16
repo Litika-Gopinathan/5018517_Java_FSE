7cc4312703ce1381dd590ca603b106b100cc8a51
Litika-Gopinathan <114471493+Litika-Gopinathan@users.noreply.github.com>
2024-08-16 16:33:39 +0530

    Add files via upload

---
+++ Week_2_Exercises/Spring Core Maven/Ex-3 Implementing Logging with Spring AOP/src/main/java/com/library/service/BookService.java
@@ -0,0 +1,16 @@
+package com.library.service;
+
+import com.library.repository.BookRepository;
+
+public class BookService {
+    private BookRepository bookRepository;
+
+    public void setBookRepository(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public void addBook() {
+        System.out.println("Adding a book to the library.");
+        bookRepository.save();
+    }
+}
